a240c53b6fca1a12654a61b575ca84dbf9e782bd
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-06 20:43:41 +0200

    done 1

--- task1.py
+++ task1.py
@@ -1,1 +1,13 @@
-input("Enter number from 1 to 100: ")
+
+user_number = int(input("Enter number from 1 to 100: "))
+if user_number not in range(1, 101):
+    print("Error! Incorrect number.")
+else:
+    if user_number % 3 == 0 and user_number % 5 == 0:
+        print("Fizz Buzz")
+    elif user_number % 3 == 0:
+        print("Fizz")
+    elif user_number % 5 == 0:
+        print("Buzz")
+    else:
+        print(user_number)
