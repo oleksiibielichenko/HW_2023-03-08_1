720b092d9748cf45eca7b37b650c41de1bd362f6
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-06 22:10:55 +0200

    not done 3

--- task3.py
+++ task3.py
@@ -1,15 +1,34 @@
 
 def operator(choose):
     if choose == '1':
-        print('KS')
+        oper = 'KS'
     elif choose == '2':
-        print('VF')
+        oper = 'VF'
     elif choose == '3':
-        print('LC')
+        oper = 'LC'
+    else:
+        return print('Incorrect operator')
+
+    return oper
 
 
 operator_1 = input("""
+Choosen your operator:
 1) KS
 2) VF
 3) LC
 """)
+operator(operator_1)
+
+operator_2 = input("""
+Choosen abonent's operator:
+1) KS
+2) VF
+3) LC
+""")
+operator(operator_2)
+
+prise = int(input("Enter prise: "))
+time = int(input("Enter call time: "))
+cost = prise*time
+print("Call from ", operator_1, " to ", operator_2, "costs: ", cost)
