f162a5530c8fb6fba5ecf9110f57b5fc2b9a73f2
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-06 21:25:39 +0200

    start 3

---
+++ task3.py
@@ -0,0 +1,15 @@
+
+def operator(choose):
+    if choose == '1':
+        print('KS')
+    elif choose == '2':
+        print('VF')
+    elif choose == '3':
+        print('LC')
+
+
+operator_1 = input("""
+1) KS
+2) VF
+3) LC
+""")
