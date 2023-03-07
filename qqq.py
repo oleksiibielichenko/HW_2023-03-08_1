6e39ab90a1775c12f5f4d4edd9c9e80c9288c62f
Oleksii Bielichenko <byelichenko@gmail.com>
2023-03-07 19:54:41 +0200

    done

--- qqq.py
+++ qqq.py
@@ -1,1 +1,11 @@
+
 def merge_lists_to_dict(list_1, list_2):
+    lists_zip = zip(list_1, list_2)
+    lists_dict = dict(lists_zip)
+    return lists_dict
+
+
+goods = ['apple', 'banana', 'strawberry', 'leamon']
+price = [15, 49, 99, 26]
+
+print(merge_lists_to_dict(goods, price))
